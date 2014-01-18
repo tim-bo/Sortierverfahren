b7b6ccf3ce813753cd5c588b0525b0460821a7dc
tim-bo <mail@tim-bo.de>
2014-01-18 12:21:49 +0100

    Add method swap()

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -4,6 +4,19 @@ import java.util.*;
 //Die Klasse Sort stellt verschiedene Methoden zur Sortierung eines Felds von int-Werten zur Verfügung...
 public class Sort 
 {
+	public static void swap(int[] list, int i, int j) throws IllegalArgumentException
+    {
+        if ((i >= list.length) || (j >= list.length) || (i < 0) || (j < 0))
+        {
+            throw new IllegalArgumentException("Ungueltige Parameter");
+        }
+        
+        int hilf = list[i];
+        list[i] = list[j];
+        list[j] = hilf; 
+    }
+	
+	
 	public static String toString(int[] list, int start, int end) throws IllegalArgumentException
 	{
 		//falls übergebene Parameter außerhalb d. Feldgrenzen o. start>end werfe Exception
@@ -32,8 +45,19 @@ public class Sort
 	 */
 	public static void main(String[] args) 
 	{
-		int[] list = new int[] {123, 140, 150, 160, 100000};
+		int[] list = new int[] {123, 140, 150, 160, 1000, 456, 21, 45};
+		System.out.println("Teste toString()");
 		System.out.println(toString(list) + " <-- int-Array als String");
+		
+		System.out.println("Teste swap()");
+		//1.Element
+		int i = 2;
+		//2.Element
+		int j = 3;
+		//Tausche 1. u. 2. Element
+		Sort.swap(list, i, j);
+		System.out.println(toString(list) + " <-- int-Array als String mit getauschen Elementen");
+		
 	}
 
 }
